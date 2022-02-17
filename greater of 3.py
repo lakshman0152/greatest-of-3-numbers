48d11e1bde3fda127cd8df01370fb70e675478cc
lu_cky <eng19cs0152.lakshmanparihar@gmail.com>
2022-02-17 13:13:25 +0530

    Add files via upload

---
+++ greater of 3.py
@@ -0,0 +1,18 @@
+print("WELCOME !\n")
+print("FIND THE LARGEST OF 3 NUM\n")
+
+user_num1 = float(input("Enter 1st number : "))
+user_num2 = float(input("Enter 2nd number : "))
+user_num3 = float(input("Enter 3rd number : "))
+
+if ( float(user_num1) > float(user_num2)) &  (float(user_num1) > float(user_num2)):
+    print( str(user_num1) + " is greater among all")
+
+elif (user_num2 >user_num3 ):
+    print(str(user_num2) +" is greater among all")
+
+else:
+    print(str(user_num3) +" is greater among all")
+
+print("")
+print("Thank you for using this application")
